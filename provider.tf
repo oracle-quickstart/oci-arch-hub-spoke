6af9c39ab639915cffca894ec2a17826c33b0152
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-04-21 13:09:23 +0200

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -3,8 +3,8 @@
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
-#  user_ocid        = var.user_ocid
-#  fingerprint      = var.fingerprint
-#  private_key_path = var.private_key_path
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
   region           = var.region
 }
