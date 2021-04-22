5f9a077edf07542fa77a5c3ab00f91ae0a5fc958
Lukasz Feldman <lukasz.feldman@oracle.com>
2021-04-22 17:32:35 +0200

    Dedicated provider resource for home region

--- datasources.tf
+++ datasources.tf
@@ -19,3 +19,12 @@ data "oci_core_images" "InstanceImageOCID" {
     regex  = true
   }
 }
+
+data "oci_identity_region_subscriptions" "home_region_subscriptions" {
+    tenancy_id = var.tenancy_ocid
+
+    filter {
+      name   = "is_home_region"
+      values = [true]
+    }
+}
